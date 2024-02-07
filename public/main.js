11c30168b34053a4f835c46b3ebe9861b0df1abb
RanjanKhadka012 <112820130+RanjanKhadka012@users.noreply.github.com>
2024-02-06 23:49:28 -0600

    stun serveres and stream cnstraints added

--- public/main.js
+++ public/main.js
@@ -1,2 +1,24 @@
+// defining necessary variables and DOM
 let divSelectRoom = document.getElementById("selectRoom")
 let divconsultingRoom = document.getElementById("consultingRoom")
+let inputRoomNumber = document.getElementById("roomNumber")
+let btnGoRoom = document.getElementById("goroom")
+let localVideo = document.getElementById("localVideo")
+let remoteVideo = document.getElementById("remoteVideo")
+
+let roomNumber, localStream, remoteStream, rtcPeerConnection, isCaller
+
+// adding ICE server
+const iceServer = {
+    'iceServer': [
+        { 'urls': 'stun:stun.services.mozilla.com' },
+        {
+            'urls': 'stun:stun.l.google.com:19302'
+        }
+    ]
+}
+
+const streamConstraints = {
+    audio: true,
+    video: true
+}
